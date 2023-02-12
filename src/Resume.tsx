15ecb38048e0f52086c9003dbd68247553c1fccd
Ravi Upadhyay <ravupad@gmail.com>
2023-02-12 15:55:26 +0530

    Stitches for styling

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,104 +1,154 @@
-import style from './resume.module.css';
-
-export const Resume = ({ resume }: { resume: IResume }) => {
-    const title = (
-        <div className="fr g-8 jc-c">
-            <h1 className="cl-pr">{resume.name}</h1>
-            <h1>|</h1>
-            <h1 className="cl-sc">Resume</h1>
-        </div>
-    );
-    const address = (
-        <div className="fr jc-c ai-c g-8 cl-sc address">
-            <span className={["material-icons", style.icons].join(' ')}>location_on</span>
-            <h4>{resume.place}</h4>
-            <span className={["material-icons", style.icons].join(' ')}>phone_iphone</span>
-            <h4>{resume.phone}</h4>
-            <span className={["material-icons", style.icons].join(' ')}>email</span>
-            <h4>{resume.email}</h4>
-        </div>
-    );
-    return (
-        <div className={['fc', style.container].join(' ')}>
-            {title}
-            {address}
-            <h3>{resume.resumeHead}</h3>
-            <Experience resume={resume} />
-            <Skill resume={resume} />
-        </div>
-    );
-}
-
-const Experience = ({ resume }: { resume: IResume }) => (
-    <div className="fc g-8">
-        <h2 className="cl-pr">Experience</h2>
-        {resume.experiences.map(experience =>
-            (<CompanyExperience experience={experience} />))}
-    </div>
-);
-
-const CompanyExperience = ({ experience }: { experience: ICompanyExperience }) => (
-    <div className="fc g-8">
-        <CompanyExperienceHeader experience={experience} />
-        {experience.lineItems.map(lineItem => (<LineItem lineItem={lineItem} />))}
-    </div>
-);
-
-const CompanyExperienceHeader = ({ experience }: { experience: ICompanyExperience }) => (
-    <div className={['fr', 'jc-sb', 'py-8', style.border].join(' ')}>
-        <div className='fc jc-sb'>
-            <div className="logo">
-                <img className={style.companyLogo} src={experience.logo} />
-            </div>
-            <h4 className="cl-sc">{experience.place}</h4>
-        </div>
-        <div className='fc cl-sc jc-sb ai-e'>
-            <h4>{experience.place}</h4>
-            <h4>{experience.from} - {experience.to}</h4>
-        </div>
-    </div>
-);
-
-const LineItem = ({ lineItem }: { lineItem: ICompanyExperienceLineItem }) => (
-    <div className="fr g-8">
-        <h1>•</h1>
-        <div>
-            <h3>{lineItem.breif}</h3>
-            <div>{lineItem.description}</div>
-        </div>
-    </div>
-);
-
-const Skill = ({ resume }: { resume: IResume }) => (
-    <div className="fc g-8">
-        <h2 className="cl-pr">Skill</h2>
-        <div className={['fr', 'jc-sb', 'g-8', 'py-8', style.border].join(' ')}>
-            {resume.skills.map(skillLogo => (<img className={style.skillLogo} src={skillLogo} />))}
-        </div>
-    </div>
-);
+import { styled, FlexRow, FlexColumn, H1, H2, H3, H4 } from './stitches.config';
 
 export interface IResume {
-    name: string;
-    place: string;
-    email: string;
-    phone: string;
-    resumeHead: string;
-    experiences: ICompanyExperience[];
-    skills: string[];
+  name: string;
+  place: string;
+  email: string;
+  phone: string;
+  resumeHead: string;
+  experiences: ICompanyExperience[];
+  skills: string[];
 }
 
 interface ICompanyExperience {
-    logo: string;
-    jobTitle: string;
-    place: string;
-    from: string;
-    to: string;
-    header: string;
-    lineItems: ICompanyExperienceLineItem[];
+  logo: string;
+  jobTitle: string;
+  place: string;
+  from: string;
+  to: string;
+  header: string;
+  lineItems: ICompanyExperienceLineItem[];
 }
 
 interface ICompanyExperienceLineItem {
-    breif: string;
-    description: string;
+  breif: string;
+  description: string;
 }
+
+export const Resume = ({ resume }: { resume: IResume }) => {
+  const title = (
+    <Title>
+      <H1>{resume.name}</H1>
+      <H1>|</H1>
+      <ResumeH1>Resume</ResumeH1>
+    </Title>
+  );
+  const address = (
+    <Address>
+      <Icon className="material-icons">location_on</Icon>
+      <H4>{resume.place}</H4>
+      <Icon className="material-icons">phone_iphone</Icon>
+      <H4>{resume.phone}</H4>
+      <Icon className="material-icons">email</Icon>
+      <H4>{resume.email}</H4>
+    </Address>
+  );
+  return (
+    <ResumeContainer>
+      {title}
+      {address}
+      <H3>{resume.resumeHead}</H3>
+      <Experience resume={resume} />
+      <Skill resume={resume} />
+    </ResumeContainer>
+  );
+}
+
+const Experience = ({ resume }: { resume: IResume }) => (
+  <FlexColumn>
+    <H2>Experience</H2>
+    {resume.experiences.map(experience =>
+      (<CompanyExperience experience={experience} />))}
+  </FlexColumn>
+);
+
+const CompanyExperience = ({ experience }: { experience: ICompanyExperience }) => (
+  <FlexColumn>
+    <CompanyExperienceHeader experience={experience} />
+    {experience.lineItems.map(lineItem => (<LineItem lineItem={lineItem} />))}
+  </FlexColumn>
+);
+
+const CompanyExperienceHeader = ({ experience }: { experience: ICompanyExperience }) => (
+  <BorderedHeader>
+    <CompanyLogo src={experience.logo} />
+    <CompanyPlaceDate>
+      <H4>{experience.place}</H4>
+      <H4>{experience.from} - {experience.to}</H4>
+    </CompanyPlaceDate>
+  </BorderedHeader>
+);
+
+const LineItem = ({ lineItem }: { lineItem: ICompanyExperienceLineItem }) => (
+  <FlexRow>
+    <H1>•</H1>
+    <FlexColumn>
+      <H3>{lineItem.breif}</H3>
+      <span>{lineItem.description}</span>
+    </FlexColumn>
+  </FlexRow>
+);
+
+const Skill = ({ resume }: { resume: IResume }) => (
+  <FlexColumn>
+    <H2>Skill</H2>
+    <BorderedHeader>
+      {
+        resume.skills.map(skillLogo => (
+          <SkillLogo src={skillLogo} />
+        ))
+      }
+    </BorderedHeader>
+  </FlexColumn>
+);
+
+const ResumeH1 = styled(H1, {
+  color: '$secondary'
+});
+
+const Address = styled(FlexRow, {
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: '$secondary',
+});
+
+const Icon = styled('span', {
+  fontSize: 20
+});
+
+const ResumeContainer = styled(FlexColumn, {
+  background: 'white',
+  gap: 16,
+  marginBlock: 24,
+  padding: 24,
+  width: 1000,
+});
+
+const SkillLogo = styled('img', {
+  objectFit: 'contain',
+  maxHeight: 32,
+});
+
+
+const BorderedHeader = styled(FlexRow, {
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingBlock: 8,
+  borderBlock: '2px dashed $primary',
+});
+
+const CompanyLogo = styled('img', {
+  objectFit: 'contain',
+  maxHeight: 36,
+  maxWidth: 100,
+});
+
+const CompanyPlaceDate = styled(FlexColumn, {
+  color: '$secondary',
+  justifyContent: 'space-between',
+  alignItems: 'end',
+});
+
+const Title = styled(FlexRow, {
+  justifyContent: 'center'
+});
