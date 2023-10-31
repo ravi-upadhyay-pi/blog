2cf59655aa327eb65ffbe9daf6df8c8681d452a1
ravi-upadhyay-pi <ravi.upadhyay.pi@gmail.com>
2023-10-31 22:17:31 +0530

    Moving out resume code

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Posts } from './blog/Posts';
-import { Resume } from './resume/Resume';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PostPage } from './blog/Post';
 import { NotFound } from './NotFound';
@@ -16,7 +15,6 @@ ReactDOM.createRoot(rootElement).render(
       <Routes>
         <Route path="/" element={<Posts />} />
         <Route path="/blog/:id" element={<PostPage />} />
-        <Route path="/resume" element={<Resume />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
